39cdf86b34603bcef4d27def8e4a73debcb5c0ab
ahmed.badar <ahmed.badar512@gmail.com>
2020-03-29 11:16:41 +0200

    added layers for hardnet

--- tensorflow2/layers.py
+++ tensorflow2/layers.py
@@ -2,42 +2,94 @@ import tensorflow as tf
 import tensorflow.keras as K
 
 
-class customSeperableConv(K.layers.Layer):
-    def __init__(self, filters, kernels, strides, padding="SAME", data_format="NHWC", dilations=1):
-        super(customSeperableConv, self).__init__()
+class convbnrelu(K.layers.Layer):
+    def __init__(self, filters, kernels=(3, 3), strides=1, padding="SAME", data_format="NHWC", dilations=1, use_bias=False):
+        super(convbnrelu, self).__init__()
         self.filters = filters
         self.kernels = kernels
         self.strides = strides
         self.padding = padding
         self.data_format = data_format
         self.dilations = dilations
+        self.use_bias = use_bias
 
     def build(self, inputs_shape):
         if self.data_format == 'NHWC':
-            self.strides = [1, self.strides, self.strides, 1]
             channel_index = -1
-        else:
-            self.strides = [1, 1, self.strides, self.strides]
+            data_format_keras = 'channels_last'
+        elif self.data_format == 'NCHW':
             channel_index = 1
-        self.w_dw = self.add_variable(name="dw_conv_w", shape=(self.kernels[0], self.kernels[1], inputs_shape[channel_index], 1))
-        self.w_pw = self.add_variable(name="dw_conv_w", shape=(1, 1, inputs_shape[channel_index], self.filters))
+            data_format_keras = 'channels_first'
+        self.bn = K.layers.BatchNormalization(axis=channel_index)
+        self.relu = K.layers.ReLU()
+        self.conv = K.layers.Conv2D(filters=self.filters,
+                                    kernel_size=self.kernels,
+                                    strides=self.strides,
+                                    padding=self.padding.lower(),
+                                    data_format=data_format_keras,
+                                    use_bias=False)
+    def call(self, inputs):
+        tensor = self.conv(inputs)
+        tensor = self.bn(tensor)
+        tensor = self.relu(tensor)
+        return tensor
+
+
+class dwconvbnrelu(K.layers.Layer):
+    def __init__(self, depth_multiplier=1, kernels=(3, 3), strides=1, padding="SAME", data_format="NHWC", dilations=1, use_bias=False):
+        super(dwconvbnrelu, self).__init__()
+        self.depth_multiplier = depth_multiplier
+        self.kernels = kernels
+        self.strides = strides
+        self.padding = padding
+        self.data_format = data_format
+        self.dilations = dilations
+        self.use_bias = use_bias
+
+    def build(self, inputs_shape):
+        if self.data_format == 'NHWC':
+            channel_index = -1
+            data_format_keras = 'channels_last'
+        elif self.data_format == 'NCHW':
+            channel_index = 1
+            data_format_keras = 'channels_first'
+        self.bn = K.layers.BatchNormalization(axis=channel_index)
+        self.relu = K.layers.ReLU()
+        self.conv = K.layers.DepthwiseConv2D(kernel_size=self.kernels,
+                                             strides=self.strides,
+                                             padding=self.padding.lower(),
+                                             data_format=data_format_keras,
+                                             depth_multiplier=self.depth_multiplier,
+                                             use_bias=False)
+    def call(self, inputs):
+        tensor = self.conv(inputs)
+        tensor = self.bn(tensor)
+        tensor = self.relu(tensor)
+        return tensor
+
+
+class combconv(K.layers.Layer):
+    def __init__(self, filters, kernels=(3, 3), strides=1, padding="SAME", data_format="NHWC", dilations=1, use_bias=False):
+        super(combconv, self).__init__()
+        self.filters = filters
+        self.kernels = kernels
+        self.strides = strides
+        self.padding = padding
+        self.data_format = data_format
+        self.dilations = dilations
+        self.use_bias = use_bias
+
+    def build(self, inputs_shape):
+        if self.data_format == 'NHWC':
+            channel_index = -1
+            data_format_keras = 'channels_last'
+        elif self.data_format == 'NCHW':
+            channel_index = 1
+            data_format_keras = 'channels_first'
+        self.dw = dwconvbnrelu(kernels=self.kernels, strides=self.strides, padding=self.padding, data_format=self.data_format)
+        self.pw = convbnrelu(self.filters, kernels=(1, 1), padding=self.padding, data_format=self.data_format)
 
     def call(self, inputs):
-         tensor = tf.nn.depthwise_conv2d(inputs,
-                                         self.w_dw,
-                                         strides=self.strides,
-                                         padding=self.padding,
-                                         data_format=self.data_format)
-         tensor = tf.nn.conv2d(tensor,
-                               filters=self.w_pw,
-                               strides=1,
-                               padding=self.padding,
-                               data_format=self.data_format)
-         return tensor
-
-
-# myLayer = customSeperableConv(10, (3, 3), 2, data_format='NCHW')
-# import numpy as np
-#
-# x = np.random.rand(1, 3, 256, 256)
-# print(myLayer(x))
+        tensor = self.pw(inputs)
+        tensor = self.dw(tensor)
+        return tensor
