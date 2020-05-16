9bb1bdbcd924658042367f1565f13e690afc868b
ahmed.badar <ahmed.badar512@gmail.com>
2020-05-16 17:36:51 +0200

    Added variational resnets

---
+++ Classification/probs_resnet.py
@@ -0,0 +1,292 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+
+class BasicBlock(tf.keras.layers.Layer):
+
+    def __init__(self, filter_num, stride=1, weight_decay=0.0001, variational=(False, True)):
+        super(BasicBlock, self).__init__()
+        # regularizer = tf.keras.regularizers.l2(weight_decay)
+        regularizer = None
+        if variational[0]:
+            self.conv1 = tfp.layers.Convolution2DReparameterization(filters=filter_num,
+                                                                    kernel_size=(3, 3),
+                                                                    strides=stride,
+                                                                    padding="same",
+                                                                    activity_regularizer=regularizer)
+        else:
+            self.conv1 = tf.keras.layers.Conv2D(filters=filter_num,
+                                                kernel_size=(3, 3),
+                                                strides=stride,
+                                                padding="same",
+                                                kernel_regularizer=regularizer,
+                                                kernel_initializer=tf.initializers.glorot_uniform())
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        if variational[1]:
+            self.conv2 = tf.keras.layers.Conv2D(filters=filter_num,
+                                                kernel_size=(3, 3),
+                                                strides=1,
+                                                padding="same",
+                                                activity_regularizer=regularizer)
+        else:
+            self.conv2 = tf.keras.layers.Conv2D(filters=filter_num,
+                                                kernel_size=(3, 3),
+                                                strides=1,
+                                                padding="same",
+                                                kernel_regularizer=regularizer,
+                                                kernel_initializer=tf.initializers.glorot_uniform())
+
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        if stride != 1:
+            self.downsample = tf.keras.Sequential()
+            self.downsample.add(tf.keras.layers.Conv2D(filters=filter_num,
+                                                       kernel_size=(1, 1),
+                                                       strides=stride))
+            self.downsample.add(tf.keras.layers.BatchNormalization())
+        else:
+            self.downsample = lambda x: x
+
+    def call(self, inputs, training=None, **kwargs):
+        residual = self.downsample(inputs)
+
+        x = self.conv1(inputs)
+        x = self.bn1(x, training=training)
+        x = tf.nn.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x, training=training)
+
+        output = tf.nn.relu(tf.keras.layers.add([residual, x]))
+
+        return output
+
+
+class BottleNeck(tf.keras.layers.Layer):
+    def __init__(self, filter_num, stride=1):
+        super(BottleNeck, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(filters=filter_num,
+                                            kernel_size=(1, 1),
+                                            strides=1,
+                                            padding='same')
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.conv2 = tf.keras.layers.Conv2D(filters=filter_num,
+                                            kernel_size=(3, 3),
+                                            strides=stride,
+                                            padding='same')
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.conv3 = tf.keras.layers.Conv2D(filters=filter_num * 4,
+                                            kernel_size=(1, 1),
+                                            strides=1,
+                                            padding='same')
+        self.bn3 = tf.keras.layers.BatchNormalization()
+
+        self.downsample = tf.keras.Sequential()
+        self.downsample.add(tf.keras.layers.Conv2D(filters=filter_num * 4,
+                                                   kernel_size=(1, 1),
+                                                   strides=stride))
+        self.downsample.add(tf.keras.layers.BatchNormalization())
+
+    def call(self, inputs, training=None, **kwargs):
+        residual = self.downsample(inputs)
+
+        x = self.conv1(inputs)
+        x = self.bn1(x, training=training)
+        x = tf.nn.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x, training=training)
+        x = tf.nn.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x, training=training)
+
+        output = tf.nn.relu(tf.keras.layers.add([residual, x]))
+
+        return output
+
+
+def make_basic_block_layer(filter_num, blocks, stride=1, variational=(False, True)):
+    res_block = tf.keras.Sequential()
+    res_block.add(BasicBlock(filter_num, stride=stride, variational=variational))
+
+    for _ in range(1, blocks):
+        res_block.add(BasicBlock(filter_num, stride=1))
+
+    return res_block
+
+
+def make_bottleneck_layer(filter_num, blocks, stride=1):
+    res_block = tf.keras.Sequential()
+    res_block.add(BottleNeck(filter_num, stride=stride))
+
+    for _ in range(1, blocks):
+        res_block.add(BottleNeck(filter_num, stride=1))
+
+    return res_block
+
+
+class ResNetTypeI(tf.keras.Model):
+    def __init__(self, layer_params, num_classes,
+                 variational_list=((True, True), (True, True), (True, True), (True, True))):
+        super(ResNetTypeI, self).__init__()
+
+        self.conv1 = tf.keras.layers.Conv2D(filters=64,
+                                            kernel_size=(7, 7),
+                                            strides=2,
+                                            padding="same")
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.pool1 = tf.keras.layers.MaxPool2D(pool_size=(3, 3),
+                                               strides=2,
+                                               padding="same")
+
+        self.layer1 = make_basic_block_layer(filter_num=64,
+                                             blocks=layer_params[0],
+                                             variational=variational_list[0])
+        self.layer2 = make_basic_block_layer(filter_num=128,
+                                             blocks=layer_params[1],
+                                             stride=2,
+                                             variational=variational_list[1])
+        self.layer3 = make_basic_block_layer(filter_num=256,
+                                             blocks=layer_params[2],
+                                             stride=2,
+                                             variational=variational_list[2])
+        self.layer4 = make_basic_block_layer(filter_num=512,
+                                             blocks=layer_params[3],
+                                             stride=2,
+                                             variational=variational_list[3])
+
+        self.avgpool = tf.keras.layers.GlobalAveragePooling2D()
+        self.fc = tf.keras.layers.Dense(units=num_classes)
+
+    def call(self, inputs, training=None, mask=None):
+        x = self.conv1(inputs)
+        x = self.bn1(x, training=training)
+        x = tf.nn.relu(x)
+        x = self.pool1(x)
+        x = self.layer1(x, training=training)
+        x = self.layer2(x, training=training)
+        x = self.layer3(x, training=training)
+        x = self.layer4(x, training=training)
+        x = self.avgpool(x)
+        output = self.fc(x)
+
+        return output
+
+
+class ResNetTypeII(tf.keras.Model):
+    def __init__(self, layer_params, num_classes):
+        super(ResNetTypeII, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(filters=64,
+                                            kernel_size=(7, 7),
+                                            strides=2,
+                                            padding="same")
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.pool1 = tf.keras.layers.MaxPool2D(pool_size=(3, 3),
+                                               strides=2,
+                                               padding="same")
+
+        self.layer1 = make_bottleneck_layer(filter_num=64,
+                                            blocks=layer_params[0])
+        self.layer2 = make_bottleneck_layer(filter_num=128,
+                                            blocks=layer_params[1],
+                                            stride=2)
+        self.layer3 = make_bottleneck_layer(filter_num=256,
+                                            blocks=layer_params[2],
+                                            stride=2)
+        self.layer4 = make_bottleneck_layer(filter_num=512,
+                                            blocks=layer_params[3],
+                                            stride=2)
+
+        self.avgpool = tf.keras.layers.GlobalAveragePooling2D()
+        self.fc = tf.keras.layers.Dense(units=num_classes)
+
+    def call(self, inputs, training=None, mask=None):
+        x = self.conv1(inputs)
+        x = self.bn1(x, training=training)
+        x = tf.nn.relu(x)
+        x = self.pool1(x)
+        x = self.layer1(x, training=training)
+        x = self.layer2(x, training=training)
+        x = self.layer3(x, training=training)
+        x = self.layer4(x, training=training)
+        x = self.avgpool(x)
+        output = self.fc(x)
+
+        return output
+
+
+class ResNetTypeIII(tf.keras.Model):
+    def __init__(self, layer_params, num_classes, weight_decay=0.0001):
+        super(ResNetTypeIII, self).__init__()
+        regularizer = tf.keras.regularizers.l2(weight_decay)
+        self.conv1 = tf.keras.layers.Conv2D(filters=16,
+                                            kernel_size=(3, 3),
+                                            strides=1,
+                                            padding="same",
+                                            kernel_regularizer=regularizer,
+                                            kernel_initializer=tf.initializers.glorot_uniform())
+        self.bn1 = tf.keras.layers.BatchNormalization()
+
+        self.layer1 = make_basic_block_layer(filter_num=16,
+                                             blocks=layer_params[0])
+        self.layer2 = make_basic_block_layer(filter_num=32,
+                                             blocks=layer_params[1],
+                                             stride=2)
+        self.layer3 = make_basic_block_layer(filter_num=64,
+                                             blocks=layer_params[2],
+                                             stride=2)
+
+        self.avgpool = tf.keras.layers.GlobalAveragePooling2D()
+        # self.fc = tf.keras.layers.Dense(units=num_classes)
+        self.fc = tfp.layers.DenseReparameterization(num_classes)
+
+    def call(self, inputs, training=None, mask=None):
+        x = self.conv1(inputs)
+        x = self.bn1(x, training=training)
+        x = tf.nn.relu(x)
+        x = self.layer1(x, training=training)
+        x = self.layer2(x, training=training)
+        x = self.layer3(x, training=training)
+        x = self.avgpool(x)
+        output = self.fc(x)
+
+        return output
+
+
+def pr_resnet_18(num_classes=10):
+    return ResNetTypeI(layer_params=[2, 2, 2, 2], num_classes=num_classes)
+
+
+def pr_resnet_34(num_classes=10):
+    return ResNetTypeI(layer_params=[3, 4, 6, 3], num_classes=num_classes)
+
+
+def pr_resnet_50(num_classes=10):
+    return ResNetTypeII(layer_params=[3, 4, 6, 3], num_classes=num_classes)
+
+
+def pr_resnet_101(num_classes=10):
+    return ResNetTypeII(layer_params=[3, 4, 23, 3], num_classes=num_classes)
+
+
+def pr_resnet_152(num_classes=10):
+    return ResNetTypeII(layer_params=[3, 8, 36, 3], num_classes=num_classes)
+
+
+def pr_resnet_20(num_classes=10, n=3):
+    return ResNetTypeIII(layer_params=[n, n, n, n], num_classes=num_classes)
+
+
+def pr_resnet_32(num_classes=10, n=5):
+    return ResNetTypeIII(layer_params=[n, n, n, n], num_classes=num_classes)
+
+
+def pr_resnet_44(num_classes=10, n=7):
+    return ResNetTypeIII(layer_params=[n, n, n, n], num_classes=num_classes)
+
+
+def pr_resnet_56(num_classes=10, n=9):
+    return ResNetTypeIII(layer_params=[n, n, n, n], num_classes=num_classes)
+
+
+if __name__ == "__main__":
+    model = pr_resnet_20()
+    model.build(input_shape=(1, 512, 512, 3))
+    print(model.summary())
