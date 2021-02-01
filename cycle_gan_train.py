df1f6a511c12e8e47c3562a0138ab88f7462431a
AhmedBadar512 <ahmed.badar512@gmail.com>
2021-02-01 16:15:11 +0100

    Added progress messagees

--- cycle_gan_train.py
+++ cycle_gan_train.py
@@ -104,16 +104,6 @@ with mirrored_strategy.scope():
     discriminator_x_optimizer = tf.keras.optimizers.Adam(LEARNING_RATE, beta_1=0.5)
     discriminator_y_optimizer = tf.keras.optimizers.Adam(LEARNING_RATE, beta_1=0.5)
 
-# ckpt = tf.train.Checkpoint(generator_g=generator_g,
-#                            generator_f=generator_f,
-#                            discriminator_x=discriminator_x,
-#                            discriminator_y=discriminator_y,
-#                            generator_g_optimizer=generator_g_optimizer,
-#                            generator_f_optimizer=generator_f_optimizer,
-#                            discriminator_x_optimizer=discriminator_x_optimizer,
-#                            discriminator_y_optimizer=discriminator_y_optimizer)
-#
-# ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
 def load_models(models_parent_dir):
     assert os.path.exists(models_parent_dir), "The path {} is not valid".format(models_parent_dir)
     p_gen_g = K.models.load_model(os.path.join(models_parent_dir, "gen_g"))
@@ -130,9 +120,6 @@ def load_models(models_parent_dir):
     print("Discriminator Y loaded successfully")
 
 
-# if ckpt_manager.latest_checkpoint:
-#     ckpt.restore(ckpt_manager.latest_checkpoint)
-#     print('Latest checkpoint restored!!')
 if load_model_path is not None:
     load_models(load_model_path)
 
