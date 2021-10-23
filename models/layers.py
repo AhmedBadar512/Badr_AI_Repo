dfa4cff78f22396dc17d1057c21c4e283062be59
AhmedBadar512 <ahmed.badar512@gmail.com>
2021-10-23 17:45:10 +0200

    Added unet with expanded conv replacement

--- models/layers.py
+++ models/layers.py
@@ -284,4 +284,5 @@ class CompDecomp(K.layers.Layer):
             return inputs
         small_inputs = tf.image.resize(inputs, tf.shape(inputs)[1:3]//self.comp_factor, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
         resized_inputs = tf.image.resize(small_inputs, tf.shape(inputs)[1:3])
-        return resized_inputs
+        final_outputs = tf.where(inputs > self.threshold, inputs, resized_inputs)
+        return final_outputs
